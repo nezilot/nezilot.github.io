3eb90fb6cee19277a04398c8e82d5c72cba01ca2
nezilot <helloword@mail.ru>
2025-05-08 18:30:19 +0500

    upd site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,75 @@
 VANTA.BIRDS({
-    el: "#background",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.0,
-    minWidth: 200.0,
-    scale: 1.0,
-    scaleMobile: 1.0,
-    color1: `#ff0101`,
-    color2: `#9500ff`,
-    colorMode: "variance",
+  el: "#background",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color1: `#ff0101`,
+  color2: `#9500ff`,
+  colorMode: "variance",
 });
 
 function typeAboutMe(index, text) {
-    const typingTextElement = document.getElementById("about-me");
+  const typingTextElement = document.getElementById("about-me");
 
-    if (index < text.length) {
-        if (text.charAt(index) === "\n") {
-            typingTextElement.innerHTML += "<br>";
-        } else {
-            typingTextElement.innerHTML += text.charAt(index);
-        }
-        index++;
-        setTimeout(() => typeAboutMe(index, text), 20);
+  if (index < text.length) {
+    if (text.charAt(index) === "\n") {
+      typingTextElement.innerHTML += "<br>";
+    } else {
+      typingTextElement.innerHTML += text.charAt(index);
     }
+    index++;
+    setTimeout(() => typeAboutMe(index, text), 20);
+  }
 }
 
 const textAboutMe =
-    "A Russian developer with expertise in C++,  \nC#, and JavaScript. I also have experience with \nframeworks like .NET, React, React Native, and Express.js";
+  "Russian developer with expertise in Go,  \nC#, and JavaScript. I also have experience with \nframeworks like .NET, Gin, React, React Native, and Express.js";
 
 typeAboutMe(0, textAboutMe);
 
 function copyToClipboard(text) {
-    navigator.clipboard.writeText(text).then(() => {
-        showNotification();
-    });
+  navigator.clipboard.writeText(text).then(() => {
+    showNotification();
+  });
 }
 
 function showNotification() {
-    const notification = document.getElementById("notification");
-    notification.style.display = "block";
-    setTimeout(() => {
-        notification.style.display = "none";
-    }, 3000);
+  const notification = document.getElementById("notification");
+  notification.style.display = "block";
+  setTimeout(() => {
+    notification.style.display = "none";
+  }, 3000);
 }
 
 document
-    .querySelector(".navbar li:nth-child(3) a")
-    .addEventListener("click", (event) => {
-        event.preventDefault();
-        copyToClipboard("x3wnl0x");
-    });
+  .querySelector(".navbar li:nth-child(3) a")
+  .addEventListener("click", (event) => {
+    event.preventDefault();
+    copyToClipboard("x3wnl0x");
+  });
 
 function typeAndErase(index, text) {
-    const title = document.getElementById("title");
-    const words = text.split(" ");
+  const title = document.getElementById("title");
+  const words = text.split(" ");
 
-    if (index < words.length) {
-        let currentText = " " + words[index];
-        title.innerHTML += currentText;
+  if (index < words.length) {
+    let currentText = " " + words[index];
+    title.innerHTML += currentText;
 
-        index++;
+    index++;
 
-        setTimeout(() => {
-            title.innerHTML = "nezilot";
+    setTimeout(() => {
+      title.innerHTML = "nezilot";
 
-            setTimeout(() => typeAndErase(index, text), 500);
-        }, 1000);
-    } else {
-        typeAndErase(0, text);
-    }
+      setTimeout(() => typeAndErase(index, text), 500);
+    }, 1000);
+  } else {
+    typeAndErase(0, text);
+  }
 }
 
 typeAndErase(0, ":D xD =D");
